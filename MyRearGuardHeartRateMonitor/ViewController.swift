eff48196fde7f6c67b232bbf5adae0267b095a15
Pratistha Sthapit <pratisthasthapit@pratisthas-macbook-pro.local>
2018-05-18 20:19:19 +1000

    Initial Commit

---
+++ MyRearGuardHeartRateMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyRearGuardHeartRateMonitor
+//
+//  Created by Pratistha Sthapit on 5/18/18.
+//  Copyright © 2018 Ashish Khadka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
